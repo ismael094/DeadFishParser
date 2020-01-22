f29b661b78626ecd126956ef9552a592354bef10
ismael094 <ismaelpitufo@gmail.com>
2020-01-22 11:10:57 +0000

    First test: red test

---
+++ src/test/java/DeadFishParser_.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class DeadFishParser_ {
+    @Test
+    public void should_return_zero_with_no_commands_string() {
+        assertThat(parser("")).isEqualTo(new int[0]);
+    }
+
+    private int[] parser(String commands) {
+        return null;
+    }
+}
