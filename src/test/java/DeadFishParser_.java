0295c3fe5717a73ae8e3b437ef35c86a0beb5d6f
ismael094 <ismaelpitufo@gmail.com>
2020-01-22 11:12:54 +0000

    Second test: red test

--- src/test/java/DeadFishParser_.java
+++ src/test/java/DeadFishParser_.java
@@ -8,6 +8,11 @@ public class DeadFishParser_ {
         assertThat(parser("")).isEqualTo(new int[0]);
     }
 
+    @Test
+    public void should_return_two_zero_with_oo_command_string() {
+        assertThat(parser("o")).isEqualTo(new int[1]);
+    }
+
     private int[] parser(String commands) {
         return new int[0];
     }
