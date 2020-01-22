2b858313dfea22afb5b4deb510a422840eb24430
ismael094 <ismaelpitufo@gmail.com>
2020-01-22 11:15:08 +0000

    Third test: red test

--- src/test/java/DeadFishParser_.java
+++ src/test/java/DeadFishParser_.java
@@ -4,13 +4,18 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class DeadFishParser_ {
     @Test
-    public void should_return_zero_with_no_commands_string() {
+    public void should_return_empty_with_no_commands_string() {
         assertThat(parser("")).isEqualTo(new int[0]);
     }
 
     @Test
-    public void should_return_two_zero_with_oo_command_string() {
+    public void should_return_one_zero_with_oo_command_string() {
         assertThat(parser("o")).isEqualTo(new int[1]);
+    }
+
+    @Test
+    public void should_return_one_with_io_command_string() {
+        assertThat(parser("io")).isEqualTo(new int[]{1});
     }
 
     private int[] parser(String commands) {
