f2093f53eafd73e311c3754dbbfd10b52f8dea46
ismael094 <ismaelpitufo@gmail.com>
2020-01-22 12:11:36 +0000

    Final refactor v2

--- src/main/java/DeadFishParser.java
+++ src/main/java/DeadFishParser.java
@@ -3,16 +3,81 @@ import java.util.List;
 
 public class DeadFishParser {
 
-    public static int[] parser(String commands) {
-        int sum = 0;
-        List<Integer> list = new ArrayList<Integer>();
-        for (int i = 0; i < commands.length(); i++) {
-            if (commands.charAt(i) == 'i') sum++;
-            else if (commands.charAt(i) == 's') sum *= sum;
-            else if (commands.charAt(i) == 'd') sum--;
-            else if (commands.charAt(i) == 'o')
-                list.add(sum);
+    public static final char INCREMENT = 'i';
+    public static final char SQUARE = 's';
+    public static final char DECREMENT = 'd';
+    public static final char OUTPUT = 'o';
+    private int sum;
+    private String command;
+    private List<Integer> list;
+
+    public DeadFishParser() {
+        sum = 0;
+        list = new ArrayList<>();
+    }
+
+    public int[] parser(String command) {
+        resetSum();
+        setCommand(command);
+
+        for (int i = 0; i < command.length(); i++) {
+            parseCommandAt(i);
         }
+        return resultAsArray();
+    }
+
+    private int[] resultAsArray() {
         return list.stream().mapToInt(i -> i).toArray();
+    }
+
+    private void parseCommandAt(int i) {
+        if (isIncrementCommand(getCharacterAt(i))) increment();
+        else if (isSquareCommand(getCharacterAt(i))) square();
+        else if (isDecrementCommand(getCharacterAt(i))) decrement();
+        else if (isOutputCommand(getCharacterAt(i))) add();
+    }
+
+    private void add() {
+        list.add(sum);
+    }
+
+    private void setCommand(String command) {
+        this.command = command;
+    }
+
+    private char getCharacterAt(int i) {
+        return command.charAt(i);
+    }
+
+    private boolean isIncrementCommand(char ch) {
+        return ch == INCREMENT;
+    }
+
+    private boolean isDecrementCommand(char ch) {
+        return ch == DECREMENT;
+    }
+
+    private boolean isSquareCommand(char ch) {
+        return ch == SQUARE;
+    }
+
+    private boolean isOutputCommand(char ch) {
+        return ch == OUTPUT;
+    }
+
+    private void resetSum() {
+        sum = 0;
+    }
+
+    private void increment() {
+        sum++;
+    }
+
+    private void decrement() {
+        sum--;
+    }
+
+    private void square() {
+        sum *= sum;
     }
 }
--- src/test/java/DeadFishParser_.java
+++ src/test/java/DeadFishParser_.java
@@ -4,39 +4,41 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class DeadFishParser_ {
 
+    private final DeadFishParser deadFishParser = new DeadFishParser();
+
     @Test
     public void should_return_empty_with_no_commands_string() {
-        assertThat(DeadFishParser.parser("")).isEqualTo(new int[0]);
+        assertThat(deadFishParser.parser("")).isEqualTo(new int[0]);
     }
 
     @Test
     public void should_return_one_zero_with_o_command_string() {
-        assertThat(DeadFishParser.parser("o")).isEqualTo(new int[1]);
+        assertThat(deadFishParser.parser("o")).isEqualTo(new int[1]);
     }
 
     @Test
     public void should_return_two_posistion_with_zero_with_oo_command_string() {
-        assertThat(DeadFishParser.parser("oo")).isEqualTo(new int[2]);
+        assertThat(deadFishParser.parser("oo")).isEqualTo(new int[2]);
     }
 
     @Test
     public void should_return_one_with_io_command_string() {
-        assertThat(DeadFishParser.parser("io")).isEqualTo(new int[]{1});
+        assertThat(deadFishParser.parser("io")).isEqualTo(new int[]{1});
     }
 
     @Test
     public void should_return_two_with_iio_command_string() {
-        assertThat(DeadFishParser.parser("iio")).isEqualTo(new int[]{2});
+        assertThat(deadFishParser.parser("iio")).isEqualTo(new int[]{2});
     }
 
     @Test
     public void should_return_nine_with_iiiso_command_string() {
-        assertThat(DeadFishParser.parser("iiiso")).isEqualTo(new int[]{9});
+        assertThat(deadFishParser.parser("iiiso")).isEqualTo(new int[]{9});
     }
 
     @Test
     public void should_return_two_with_iiido_command_string() {
-        assertThat(DeadFishParser.parser("iiido")).isEqualTo(new int[]{2});
+        assertThat(deadFishParser.parser("iiido")).isEqualTo(new int[]{2});
     }
 
 }
